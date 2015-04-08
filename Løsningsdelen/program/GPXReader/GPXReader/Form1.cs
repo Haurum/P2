13052fd61d04838ad35bf011724f37991786fb74
Mark Haurum <mark@markhaurum.com>
2015-04-09 01:46:48 +0200

    GPXReader

---
+++ Løsningsdelen/program/GPXReader/GPXReader/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPXReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadGPXButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            Stream GpxStream = null;
+            ofd.Filter = "GPX files (*.gpx) |*.gpx";
+            DialogResult result = ofd.ShowDialog(); // Show the dialog.
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((GpxStream = ofd.OpenFile()) != null)
+                    {
+                        GpxReader reader = new GpxReader(GpxStream);
+                        MessageBox.Show(reader.Route.RoutePoints.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
