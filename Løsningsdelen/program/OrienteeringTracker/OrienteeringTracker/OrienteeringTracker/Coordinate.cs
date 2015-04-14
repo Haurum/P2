0d3a933407437e82d82cd7bfaa102852a94cf316
Haurum <mark@markhaurum.com>
2015-04-14 10:53:50 +0200

    The gpxreader

--- Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Coordinate.cs
+++ Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Coordinate.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace OrienteeringTracker
 {
     class Coordinate
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        public Coordinate(float x, float y, DateTime t)
+        {
+            _p = new PointF(x, y);
+            Time = t;
+        }
+
+        private PointF _p { get; set; }
+        public PointF p
+        {
+            get
+            {
+                return _p;
+            }
+        }
         public DateTime Time { get; set; }
         
     }
