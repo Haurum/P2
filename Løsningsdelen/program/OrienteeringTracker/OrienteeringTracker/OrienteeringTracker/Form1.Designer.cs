865fcf728880bb93ac2d09464874f225729f6398
bovle <frederik.bl@live.dk>
2015-04-15 11:15:32 +0200

    buttons

--- Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Form1.Designer.cs
+++ Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.Map1 = new System.Windows.Forms.PictureBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Map1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,11 +48,31 @@
             this.Map1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Map1_MouseMove);
             this.Map1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Map1_MouseWheel);
             // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(584, 344);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(503, 344);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(75, 23);
+            this.PlayButton.TabIndex = 2;
+            this.PlayButton.Text = "Play/Pause";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(671, 379);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.LoadButton);
             this.Controls.Add(this.Map1);
             this.Name = "MainForm";
             this.Text = "Form1";
@@ -62,6 +84,8 @@
         #endregion
 
         public System.Windows.Forms.PictureBox Map1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button PlayButton;
     }
 }
 
