885cf503075b023f9526ece49690c04242433d5e
Haurum <mark@markhaurum.com>
2015-04-17 10:05:13 +0200

    Andet shit

--- Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Form1.cs
+++ Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker/Form1.cs
@@ -23,6 +23,7 @@ namespace OrienteeringTracker
 
         Color[] Colors = { Color.Blue, Color.Red, Color.Black, Color.Purple, Color.Turquoise, Color.Orange };
         List<Route> Routes = new List<Route>();
+        List<ControlPoint> ControlPoints = new List<ControlPoint>();
         private Bitmap OriginalMap;
         private int MousePosX, MousePosY;
         private float ZoomFactor = 1;
@@ -195,6 +196,63 @@ namespace OrienteeringTracker
             ResetButton.Hide();
             LoadButton.Show();
             RunnersCheckBox.Items.Clear();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult dr = ofd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                ControlPoints = Helper.ReadControlPoints(ofd.FileName);
+            }
+
+            Graphics g = Graphics.FromImage(Map1.Image);
+            Pen p = new Pen(Color.Magenta);
+            p.Width = 5;
+            int i = 0;
+
+            foreach (ControlPoint cp in ControlPoints)
+            {
+                Point[] points = {new Point(Convert.ToInt32(cp.Cord.p.X + 30), Convert.ToInt32(cp.Cord.p.Y)), 
+                                         new Point(Convert.ToInt32(cp.Cord.p.X - 15), Convert.ToInt32(cp.Cord.p.Y-30)), 
+                                         new Point(Convert.ToInt32(cp.Cord.p.X - 15), Convert.ToInt32(cp.Cord.p.Y+30))};
+                if (ControlPoints.First() == cp)
+                {
+                    g.DrawPolygon(p, points);
+                }
+                else if (ControlPoints.Last() == cp)
+                {
+                    g.DrawEllipse(p, cp.Cord.p.X - 17, cp.Cord.p.Y - 17, 34, 34);
+                    g.DrawEllipse(p, cp.Cord.p.X - 25, cp.Cord.p.Y - 25, 50, 50);
+                }
+                else
+                {
+                    g.DrawEllipse(p, cp.Cord.p.X - 25, cp.Cord.p.Y - 25, 50, 50);
+                }
+
+
+                using (Font myFont = new Font("Arial", 24, FontStyle.Bold))
+                {
+                    g.DrawString(cp.Number.ToString(), myFont, Brushes.Magenta, new Point(Convert.ToInt32(cp.Cord.p.X + 30), Convert.ToInt32(cp.Cord.p.Y - 25 / 2)));
+                }
+
+                if (ControlPoints.First() != cp)
+                {
+                    float xDiff = ControlPoints[i - 1].Cord.p.X - cp.Cord.p.X;
+                    float yDiff = ControlPoints[i - 1].Cord.p.Y - cp.Cord.p.Y;
+
+                    float angle = (float)Math.Atan2(yDiff, xDiff) - (float)(Math.PI);
+
+                    float distance = (float)Math.Sqrt(xDiff * xDiff + yDiff * yDiff);
+
+                    float newX = (float)(ControlPoints[i - 1].Cord.p.X + (Math.Cos(angle) * (distance - 25)));
+                    float newY = (float)(ControlPoints[i - 1].Cord.p.Y + (Math.Sin(angle) * (distance - 25)));
+
+                    g.DrawLine(p, new Point(Convert.ToInt32(ControlPoints[i - 1].Cord.p.X + Math.Cos(angle) * 25), Convert.ToInt32(ControlPoints[i - 1].Cord.p.Y + Math.Sin(angle) * 25)), new Point(Convert.ToInt32(newX), Convert.ToInt32(newY)));
+                }
+                i++;
+            }
         }
 
 
