04509c699d862fa8ee98bf4425d0a567354161d5
Haurum <mark@markhaurum.com>
2015-05-22 09:38:26 +0200

    Testy testy

--- Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker.Test/HelperTest.cs
+++ Løsningsdelen/program/OrienteeringTracker/OrienteeringTracker/OrienteeringTracker.Test/HelperTest.cs
@@ -10,6 +10,24 @@ namespace OrienteeringTracker.Test
     [TestFixture]
     public class HelperTest
     {
+        [TestCase]
+        public void CalcSingleLength_TwoPoints_Return5()
+        {
+            double dist = Helper.CalcSingleLength(-2, 1, 1, 5);
+            Assert.AreEqual(dist, 5);
+        }
+   
+        [TestCase]
+        public void ConvertLatLongtoUTM_LatLong_UTM()
+        {
+            double easting;
+            double northing;
+            string zone;
+            Helper.ConvertLatLongtoUTM(57.121332, 9.953613, out northing, out easting, out zone);
 
+            Assert.AreEqual(Math.Round(easting, 2), Math.Round(557740.21, 2));
+            Assert.AreEqual(Math.Round(northing, 2), Math.Round(6331295.72, 2));
+            Assert.AreEqual(zone, "32V");
+        }
     }
 }
